25008fc43e74d69fe910b9c970037a63d6c1ebfb
Neilcen Pedrosa <cen.pedrosa08@gmail.com>
2025-09-19 13:28:33 +0800

    initial

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,6 +16,23 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+           /* Algorithm (in order):
+        a. Traverse the array up until the last element
+        b. Compare (using the bool compare() function of Entry) the `entry` to be added to the `existing entry` currently traversed.
+        c. To get the current entry, you can access it via `array[i * sizeof(Entry)]` or `*(array + i * sizeof(Entry)). I will discuss first why is this the case.
+        d. If `entry` to be added is indeed greater than the existing one, `SHIFT RIGHT`
+        e. After shift right, add the new entry to the correct position.
+
+    Things to consider:
+        - You can still add an entry EVEN IF the array is FULL for as long as the newly added array is greater than most scores in the current boardarray
+        - IF ARRAY IS FULL and the SCORE IS TOO LOW TO BE ADDED TO BOARDARRAY, print "<name>'s score is too low to be added!".
+        - You can test in the main() function and interact with it to test your code before running full unit test
+            */
+            for(int i = 0; i < index, i++){
+                array[i].compare();
+                if(entry -> compare array[i]);
+            }
+            }
             return;
         }
 
